3b725e6ec8f578b553a7ba31c71ea7e9bfd7c346
Juan Savedra <juansavedra827@gmail.com>
2022-12-07 19:25:50 -0300

    listing vacancies

--- src/components/comuns/Vacancy.vue
+++ src/components/comuns/Vacancy.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="card">
-    <div class="card-header bg-dark text-white">Título da vaga</div>
+    <div class="card-header bg-dark text-white">{{title}}</div>
     <div class="card-body">
-      <p>Descrição da vaga...</p>
+      <p>{{description}}</p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Salário: R$6.000,00 | Modalidade: Home Office | Tipo: CLT | Publicação: 06/12/2022</small>
+      <small class="text-muted">
+        Salário: R${{salary}} | Modalidade: {{model}} | Tipo: {{type}} | Publicação: {{publicationDate}}
+      </small>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "Vacancy"
+    name: "Vacancy",
+    props: ['title', 'description', 'salary', 'model', 'type', 'publicationDate']
   }
 </script>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -5,22 +5,42 @@
         <SearchVacancy />
       </div>
     </div>
-
-    <div class="row mt-5">
+    <div class="row mt-5" v-for="(vacancy, index) in vacancys" v-bind:key="index">
       <div class="col">
-        <Vacancy />
+        <Vacancy 
+          v-bind:title="vacancy.title"
+          v-bind:description="vacancy.description" 
+          v-bind:salary="vacancy.salary" 
+          v-bind:model="vacancy.model" 
+          v-bind:type="vacancy.type" 
+          v-bind:publicationDate="vacancy.publicationDate" 
+        />
       </div>
     </div>
-
     <div class="row mt-5">
       <div class="col-4">
-        <Indicator title="Vagas abertas" indicator="100" bg="bg-dark" color="text-white"/>
+        <Indicator 
+          title="Vagas abertas" 
+          indicator="100" 
+          bg="bg-dark" 
+          color="text-white"
+        />
       </div>
       <div class="col-4">
-        <Indicator title="Profissionais cadastrados" indicator="225" bg="bg-dark" color="text-white" />
+        <Indicator 
+          title="Profissionais cadastrados"
+          indicator="225" 
+          bg="bg-dark" 
+          color="text-white" 
+        />
       </div>
       <div class="col-4">
-        <Indicator title="Visitantes online" v-bind:indicator="usersOnline" bg="bg-light" color="text-dark" />
+        <Indicator 
+          title="Visitantes online" 
+          v-bind:indicator="usersOnline" 
+          bg="bg-light" 
+          color="text-dark" 
+        />
       </div>
     </div>
   </div>
@@ -39,7 +59,54 @@
       Vacancy
     },
     data: () => ({
-      usersOnline: 0
+      usersOnline: 0,
+      vacancys: [
+        {
+          id: 1,
+          title: 'Analista Programador PHP Pleno',
+          description: 'Profissional com conhecimentos em PHP, Laravel e MySQL. Necessário 3 anos de experiências. Atuará na manutenção de sistemas legados da empresa.',
+          salary: 6000,
+          model: 'Home Office',
+          type: 'PJ',
+          publicationDate: '2021-10-10'
+        },
+        {
+          id: 2,
+          title: 'Programador JavaScript Angular',
+          description: 'Profissional com conhecimentos avançados em JavaScript e Angular.',
+          salary: 5000,
+          model: 'Presencial',
+          type: 'CLT',
+          publicationDate: '2021-10-07'
+        },
+        {
+          id: 3,
+          title: 'Programador JavaScript Vue',
+          description: 'Profissional com conhecimentos avançados em JavaScript e Vue.',
+          salary: 5000,
+          model: 'Home Office',
+          type: 'CLT',
+          publicationDate: '2021-10-06'
+        },
+        {
+          id: 4,
+          title: 'Analista de Banco de Dados Sênior',
+          description: 'Domínio dos bancos de dados SQL Server, Oracle, Postgre e MySQL',
+          salary: 9000,
+          model: 'Presencial',
+          type: 'PJ',
+          publicationDate: '2021-10-06'
+        },
+        {
+          id: 5,
+          title: 'Programador Web Júnior',
+          description: 'Conhecimentos básicos em HTML, CSS, JavaScript, Bootstrap, PHP e MySQL',
+          salary: 3000,
+          model: 'Presencial',
+          type: 'CLT',
+          publicationDate: '2021-10-05'
+        }
+      ]
     }),
     methods: {
       getUsersOnline() {
